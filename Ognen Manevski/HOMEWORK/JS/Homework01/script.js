f48ca464df13d9441d995b9d7beae59d7125ce6c
Ognen Manevski <ognen.manevski@gmail.com>
2025-12-18 00:07:20 +0100

    finished homework 01 from JS course

--- Ognen Manevski/HOMEWORK/JS/Homework01/script.js
+++ Ognen Manevski/HOMEWORK/JS/Homework01/script.js
@@ -1,10 +1,19 @@
 // calculate the price of 30 phones where the price of one phone is $119.95 and the tax rate is 5%
 
-const phonePrice = 119.95;
-const taxRate = 0.05;
-const numberOfPhones = 30;
+let phonePrice = 119.95;
+let taxRate = 5;
+let numberOfPhones = 30;
 
-const totalPriceBeforeTax = phonePrice * numberOfPhones;
-const taxAmount = totalPriceBeforeTax * taxRate;
-const totalPriceAfterTax = totalPriceBeforeTax + taxAmount;
-console.log("Total price after tax: $" + totalPriceAfterTax.toFixed(2));
+//calculating tax rate in percentage
+let taxRatePercent = taxRate / 100;
+
+//receipt with calculations
+
+let totalPriceBeforeTax = phonePrice * numberOfPhones;
+console.log(`Total price before tax: $${totalPriceBeforeTax.toFixed(2)}`); //2 decimal places + rounding up
+
+let taxAmount = totalPriceBeforeTax * taxRatePercent;
+console.log(`Total tax amount: $${taxAmount.toFixed(2)} (5%)`);
+
+let totalPriceAfterTax = totalPriceBeforeTax + taxAmount;
+console.log(`Total price after tax: $${totalPriceAfterTax.toFixed(2)}`);
