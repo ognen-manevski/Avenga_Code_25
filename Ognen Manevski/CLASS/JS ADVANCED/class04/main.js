0efa9a24e7ac09d4e9086e04af38fd024b2dd896
Ognen <ognen.manevski@gmail.com>
2026-02-06 18:12:23 +0100

    Update JS Advanced class04

---
+++ Ognen Manevski/CLASS/JS ADVANCED/class04/main.js
@@ -0,0 +1,12 @@
+//functions in js
+function sayHello(){
+    console.log("Hello from my function");    
+}
+// sayHello();
+
+function sumTwoNumbers(a,b){
+    return a+b;
+}
+// sumTwoNumbers(5,10);
+
+//anonymous functions
