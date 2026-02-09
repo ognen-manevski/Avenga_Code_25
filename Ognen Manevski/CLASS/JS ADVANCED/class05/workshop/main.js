fbc7557f37691483264b874445f2ccd6fb112a95
Ognen <ognen.manevski@gmail.com>
2026-02-09 20:51:06 +0100

    workshop01

---
+++ Ognen Manevski/CLASS/JS ADVANCED/class05/workshop/main.js
@@ -0,0 +1,65 @@
+//----------------------------------------------------------------------
+// #region Workshop
+//----------------------------------------------------------------------
+
+// Build your app 🥽
+// CountrySearch 🌍🌎🌏
+// CountrySearch is an application that is meant to search for countries and get countries for them in real-time. The application is very simple. It only has one functionality: Search and show the countries in a table
+
+// Requirements
+// There should be one search input to input the name or partial name of a country
+// There should be a button for search to initiate
+// When the button is clicked, a list of cards shows the possible answers
+// The list shows:
+// Flag
+// Name
+// Population
+// Capital
+// Link to the country's Wikipedia page "Open on Wikipedia" (https://en.wikipedia.org/wiki/< country_name>)
+// The API for countries is: https://restcountries.com/
+// Read the API documentation to figure out how to call for the countries
+// Extra requirements
+// Add three buttons on top of the list with the following functionality:
+
+// Button 1: Get all countries in Europe
+// Button 2: Get all countries that are neighbors of Macedonia
+// Button 3: Get Macedonia
+
+// #endregion
+//----------------------------------------------------------------------
+
+//----------------------------------------------------------------------
+// #region 🧩 Code-Level Example Copilot Instructions (Paste in JS File as Comments)
+//----------------------------------------------------------------------
+
+// 1. select all the html elements needed
+// 2. implement functon to fetch data (provide fields what kind of data we need)
+// 3. implement a function to display/hide spinner
+// 4. implement function that will display data in cards
+// 5. function that will display data in table
+// 6. implement 5 event listeners for the 5 btns
+// 6.1 search, reset, all from europe, all neighbors of Macedonia, Macedonia
+// 7. implement constructior function with props only needed for the cards (flag, name, population, capital, wiki link)
+
+
+//
+
+// Create a function that fetches countries based on search input
+// Create a function that renders country cards into the DOM
+// Add event listener for search button
+// Add event listener for 'Europe' button
+// Add event listener for 'Macedonia' button
+// Add event listener for 'Neighbors of Macedonia' button
+// Handle API errors and empty results gracefully
+
+// #endregion
+//----------------------------------------------------------------------
+
+//----------------------------------------------------------------------
+// HTML
+//----------------------------------------------------------------------
+
+// #endregion
+//----------------------------------------------------------------------
+
+
