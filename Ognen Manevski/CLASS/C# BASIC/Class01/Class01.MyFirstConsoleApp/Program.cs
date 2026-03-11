9a88f72007373a150b40c2d65226d88dad1c5f46
Ognen <ognen.manevski@gmail.com>
2026-03-11 20:08:51 +0100

    123

---
+++ Ognen Manevski/CLASS/C# BASIC/Class01/Class01.MyFirstConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Class01.MyFirstConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
